93ece7c7374855c31aea03ea7cc24b7d36065977
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-10-04 16:33:06 -0500

    Update main.js

--- Source/main.js
+++ Source/main.js
@@ -1,1 +1,10 @@
-console.log("yeet");
+const app = express()
+const port = 443
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
